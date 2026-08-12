fd62101917cacdb8f8da948e778ec703a363fc5b
stevehizmi <36041377+stevehizmi@users.noreply.github.com>
2026-08-12 16:30:18 -0500

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            key = tuple(sorted(self._convert_to_dict(s).items()))
+            res[key].append(s)
+        return list(res.values())
+
+    def _convert_to_dict(self, string: str) -> dict:
+        if not string:
+            return {}
+
+        hist = {}
+        for letter in string:
+            if letter not in hist:
+                hist[letter] = 1
+            else:
+                hist[letter] += 1
+        return hist
+
+    def _is_anagram(self, string1: str, string2: str) -> bool:
+        return self._convert_to_dict(string1) == self._convert_to_dict(string2)
